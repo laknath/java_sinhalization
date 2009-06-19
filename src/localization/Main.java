6c6da26c39aa5c5c388eaa8f094f539097000b14
unknown <Buddhika Laknath@.(none)>
2009-06-19 17:30:33 +0600

    new package

---
+++ src/localization/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package localization;
+/**
+ *
+ * @author zam
+ */
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+         new Inputs().setVisible(true);
+           
+       
+    }
+
+}
